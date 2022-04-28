a1878b7d9f4bd452687631854588850a2140c391
Ladylane <ladylanesousa@gmail.com>
2022-04-28 17:48:59 -0300

    [Main Group 22][Todos] Tela do Explore Drinks or Foods - Requisitos 70 ao 74

--- src/pages/ExploreFoods/index.js
+++ src/pages/ExploreFoods/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../../Components/Header';
 import FooterMenu from '../../Components/FooterMenu';
+import SurpriseButton from '../../Components/SurpriseButton';
 
 const ExploreFoods = ({ title }) => (
   <>
     <Header title="Explore Foods" />
     <h1>{title}</h1>
+    <Link to="/explore/foods/ingredients">
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        By Ingredient
+      </button>
+    </Link>
+    <Link to="/explore/foods/nationalities">
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+      >
+        By Nationality
+      </button>
+    </Link>
+    <SurpriseButton
+      endpoint="https://www.themealdb.com/api/json/v1/1/random.php"
+      page="Foods"
+    />
     <FooterMenu />
   </>
 );
-
 ExploreFoods.propTypes = {
   title: propTypes.string,
 }.isRequired;

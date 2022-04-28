20f0008840718bf2e964ccbb9e11be9493ecf23e
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-04-28 14:31:13 -0300

    [Main Group 22][Todos] Tela de Profile - ok

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import { func } from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import FooterMenu from '../../Components/FooterMenu';
 import Header from '../../Components/Header';
 
-const Profile = () => (
-  <>
-    <Header title="Profile" />
-    <h1>Profile</h1>
-    <FooterMenu />
-  </>
-);
+const Profile = () => {
+  const history = useHistory();
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  return (
+    <>
+      <Header title="Profile" />
+      <h1 data-testid="profile-email">
+        <strong>
+          { email }
+        </strong>
+      </h1>
+      <button
+        type="button"
+        onClick={ () => history.push('/done-recipes') }
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        onClick={ () => history.push('/favorite-recipes') }
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
+      <FooterMenu />
+    </>
+  );
+};
+
+Profile.propTypes = {
+  push: func,
+}.isRequired;
 
 export default Profile;

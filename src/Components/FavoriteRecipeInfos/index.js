ce213eeb3bb29916558a3d8e7b940e306cecc6c9
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-05-11 22:03:44 -0300

    [main-group-22]style

--- src/Components/FavoriteRecipeInfos/index.js
+++ src/Components/FavoriteRecipeInfos/index.js
@@ -4,6 +4,7 @@ import propTypes from 'prop-types';
 import blackHeartIcon from '../../images/blackHeartIcon.svg';
 import shareIcon from '../../images/shareIcon.svg';
 import MyContext from '../../Context/MyContext';
+import FavoriteCar from './style';
 
 const copy = require('clipboard-copy');
 
@@ -32,57 +33,68 @@ const FavoriteRecipeInfos = () => {
     const page = item.type === 'food' ? 'Foods' : 'Drinks';
 
     return (
-      <div key={ item.id }>
-        <Link to={ `/${page.toLowerCase()}/${item.id}` }>
-          <h2 data-testid={ `${index}-horizontal-name` }>{item.name}</h2>
-          <img
-            data-testid={ `${index}-horizontal-image` }
-            src={ item.image }
-            alt={ item.name }
-            width="100"
-          />
-        </Link>
-        {
-          page === 'Foods'
-            ? (
-              <span data-testid={ `${index}-horizontal-top-text` }>
-                {`${item.nationality} - ${item.category}`}
-              </span>
-            )
-            : (
-              <span data-testid={ `${index}-horizontal-top-text` }>
-                {`${item.alcoholicOrNot}`}
-              </span>
-            )
-        }
-        <button
-          data-testid={ `${index}-horizontal-favorite-btn` }
-          type="button"
-          id={ item.id }
-          onClick={ ({ target: { id } }) => handleFavoriteClick(id) }
-          src={ blackHeartIcon }
-        >
-          <img
-            src={ blackHeartIcon }
-            alt="Favorite icon"
-          />
-        </button>
-        {
-          isCopied ? (<span>Link copied!</span>) : (
+
+      <FavoriteCar key={ item.id }>
+        <div>
+          <Link to={ `/${page.toLowerCase()}/${item.id}` }>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ item.image }
+              alt={ item.name }
+              width="100"
+            />
+            <section>
+              <h2 data-testid={ `${index}-horizontal-name` }>{item.name}</h2>
+              {
+                page === 'Foods'
+                  ? (
+                    <span data-testid={ `${index}-horizontal-top-text` }>
+                      {`${item.nationality} - ${item.category}`}
+                    </span>
+                  )
+                  : (
+                    <span data-testid={ `${index}-horizontal-top-text` }>
+                      {`${item.alcoholicOrNot}`}
+                    </span>
+                  )
+              }
+            </section>
+          </Link>
+        </div>
+
+        <section>
+          <div>
+
             <button
-              data-testid={ `${index}-horizontal-share-btn` }
+              data-testid={ `${index}-horizontal-favorite-btn` }
               type="button"
-              src={ shareIcon }
-              onClick={ () => {
-                copy(window.location.href
-                  .replace('/favorite-recipes', `/${page.toLowerCase()}/${item.id}`));
-                setIsCopied(true);
-              } }
+              id={ item.id }
+              onClick={ ({ target: { id } }) => handleFavoriteClick(id) }
+              src={ blackHeartIcon }
             >
-              <img src={ shareIcon } alt="Share icon" />
-            </button>)
-        }
-      </div>
+              <img
+                src={ blackHeartIcon }
+                alt="Favorite icon"
+              />
+            </button>
+            {
+              isCopied ? (<span>Link copied!</span>) : (
+                <button
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  type="button"
+                  src={ shareIcon }
+                  onClick={ () => {
+                    copy(window.location.href
+                      .replace('/favorite-recipes', `/${page.toLowerCase()}/${item.id}`));
+                    setIsCopied(true);
+                  } }
+                >
+                  <img src={ shareIcon } alt="Share icon" />
+                </button>)
+            }
+          </div>
+        </section>
+      </FavoriteCar>
     );
   });
 };
